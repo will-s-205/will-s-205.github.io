4402c0793c6974150b7a49e4f3c3040c8e5736ee
will-s-205-mac <will.step.205@gmail.com>
2022-01-09 18:30:31 -0800

    fight-scene-2 auto battle added

--- game-dev/fight-scene/fight-scene-2/main.js
+++ game-dev/fight-scene/fight-scene-2/main.js
@@ -1,15 +1,25 @@
+var r = Math.random(100);
+
 var gameData = {
-    health1: 100,
-    health2: 100,
-    dmgPerClick: 7
-  }
+  health1: 100,
+  health2: 100,
+  dmgPerClick: 3
+}
 
-  function fight1() {
-    gameData.health1 -= gameData.dmgPerClick
-    document.getElementById("health").innerHTML = gameData.health1 + " Health"
-  }
+function fight1() {
+  gameData.health1 -= gameData.dmgPerClick
+  document.getElementById("health").innerHTML = gameData.health1 + " Health"
+}
 
-  function fight2() {
-    gameData.health2 -= gameData.dmgPerClick
-    document.getElementById("health2").innerHTML = gameData.health2 + " Health"
-  }
+function fight2() {
+  gameData.health2 -= gameData.dmgPerClick
+  document.getElementById("health2").innerHTML = gameData.health2 + " Health"
+}
+
+// AUTOMATICAL FIGHTING
+var mainGameLoop = window.setInterval(function () {
+  if (Math.random() * 100 < 50) {
+    fight1();
+  } 
+  else {fight2()}
+}, 1000)
