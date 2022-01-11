3fbe2a8267d812b1900eecbbe1c871f6bcb7217d
will-s-205-mac <will.step.205@gmail.com>
2022-01-10 22:15:16 -0800

    Automatic fighting updated
    Update main.js
    
    Update main.js

--- game-dev/fight-scene-2/main.js
+++ game-dev/fight-scene-2/main.js
@@ -1,5 +1,3 @@
-var r = Math.random(100);
-
 var gameData = {
   health1: 100,
   health2: 100,
@@ -16,10 +14,36 @@ function fight2() {
   document.getElementById("health2").innerHTML = gameData.health2 + " Health"
 }
 
-// AUTOMATICAL FIGHTING
+// AUTOMATIC FIGHTING
+// var mainGameLoop = window.setInterval(function () {
+//   if (Math.random() * 100 < 50) {
+//     fight1();
+//   } 
+//   else {fight2()}
+// }, 1000)
+
+var playerOne = {
+  accuracy: 70
+}
+
+var playerTwo = {
+  defense: 50
+}
+
+var random = Math.random() * 100
+var hitChance = (playerOne.accuracy - playerTwo.defense)
+var hitChance = (playerOne.accuracy - playerTwo.defense)
+
+function fight() {
+  console.log(Math.random() * 100);
+  console.log(hitChance);
+  if (Math.random() * 100 < hitChance){
+    gameData.health2 -= gameData.dmgPerClick;
+    document.getElementById("health2").innerHTML = gameData.health2 + " Health";
+  }
+}
+
+// AUTOMATIC FIGHTING
 var mainGameLoop = window.setInterval(function () {
-  if (Math.random() * 100 < 50) {
-    fight1();
-  } 
-  else {fight2()}
+  fight()
 }, 1000)
