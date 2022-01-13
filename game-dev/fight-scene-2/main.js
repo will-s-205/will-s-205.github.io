4ef65ab79992e22d0de89bdf044df07c9e7beec3
will-s-205-mac <will.step.205@gmail.com>
2022-01-12 16:08:26 -0800

    fight logic improved

--- game-dev/fight-scene-2/main.js
+++ game-dev/fight-scene-2/main.js
@@ -15,9 +15,34 @@ function fight2() {
 }
 
 // AUTOMATIC FIGHTING
+// var mainGameLoop = window.setInterval(function () {
+//   if (Math.random() * 100 < 50) {
+//     fight1();
+//   } 
+//   else {fight2()}
+// }, 1000)
+
+var playerOne = {
+  accuracy: 70
+}
+
+var playerTwo = {
+  defense: 50
+}
+
+var random = Math.random() * 100
+var hitChance = (playerOne.accuracy - playerTwo.defense)
+
+function fight() {
+  console.log(Math.random() * 100);
+  console.log("HIT CHANCE: "+hitChance);
+  if (Math.random() * 100 < hitChance){
+    gameData.health2 -= gameData.dmgPerClick;
+    document.getElementById("health2").innerHTML = gameData.health2 + " Health";
+  }
+}
+
+// AUTOMATIC FIGHTING
 var mainGameLoop = window.setInterval(function () {
-  if (Math.random() * 100 < 50) {
-    fight1();
-  } 
-  else {fight2()}
+  fight()
 }, 1000)
