153789dcd184374cc6cd7a424c0ffa860ebdc550
will-s-205-mac <will.step.205@gmail.com>
2022-01-12 18:07:22 -0800

    dmg acc health bar changes

--- game-dev/fight-scene-2/main.js
+++ game-dev/fight-scene-2/main.js
@@ -1,3 +1,6 @@
+var random = Math.random() * 100
+var randomFloor = Math.floor(Math.random() * 100)
+
 var gameData = {
   health1: 100,
   health2: 100,
@@ -22,27 +25,58 @@ function fight2() {
 //   else {fight2()}
 // }, 1000)
 
-var playerOne = {
-  accuracy: 70
-}
+// NEW FIGHTING LOGIC
 
+//EQUIPMENT
+var playerOne = {
+  accuracy: 70,
+  defense: 20
+}
 var playerTwo = {
+  accuracy: 70,
   defense: 50
 }
-
-var random = Math.random() * 100
-var hitChance = (playerOne.accuracy - playerTwo.defense)
-
-function fight() {
-  console.log(Math.random() * 100);
-  console.log("HIT CHANCE: "+hitChance);
-  if (Math.random() * 100 < hitChance){
-    gameData.health2 -= gameData.dmgPerClick;
-    document.getElementById("health2").innerHTML = gameData.health2 + " Health";
-  }
+var sword = {
+  swordAcc: 20,
+  swordDmg: 10
 }
+var bow = {
+  bowAcc: 5,
+  bowDmg: 5
+}
+var shield = {
+  shieldDef: 20
+}
+
+// ACCURACY CALCULATIONS
+var hitChancePlayerOne = (playerOne.accuracy + sword.swordAcc - playerTwo.defense)
+var hitChancePlayerTwo = (playerTwo.accuracy + bow.bowAcc - playerOne.defense - shield.shieldDef)
+
+// FIGHT FLOW
+function fight() {
+  if (Math.random() * 100 < hitChancePlayerOne) {
+    console.log("SWORDSMAN HIT CHANCE: " + hitChancePlayerOne);
+    gameData.health2 -= sword.swordDmg;
+    document.getElementById("health2").innerHTML = gameData.health2 + " Health";
+
+    // HEALTH BAR
+    document.getElementById("healthbar1")
+      .style.width = gameData.health1 + "%"
+  }
+  if (Math.random() * 100 < hitChancePlayerTwo) {
+    console.log("ARCHER HIT CHANCE: " + hitChancePlayerTwo);
+    gameData.health1 -= bow.bowDmg;
+    document.getElementById("health").innerHTML = gameData.health1 + " Health";
+  }
+} // MAKE HIT OR MISS CONSOLE.LOG AND HIT CHANCE IN THE BRACKET()
 
 // AUTOMATIC FIGHTING
 var mainGameLoop = window.setInterval(function () {
   fight()
-}, 1000)
+}, 100)
+
+// THREAD.SLEEP
+function sleep(seconds) {
+  var e = new Date().getTime() + (seconds * 1000);
+  while (new Date().getTime() <= e) { }
+}
