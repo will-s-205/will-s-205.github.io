b01934885c2205c5d12c1aee8122665b5a240032
will-s-205-mac <will.step.205@gmail.com>
2022-08-02 01:05:12 -0700

    animation added

--- game-dev/fight-scene-3/main.js
+++ game-dev/fight-scene-3/main.js
@@ -1,23 +1,37 @@
 var gameData = {
-    health1: 100,
-    health2: 100,
-    dmgPerClick: 3
-  }
-  
-  function fight1() {
-    gameData.health1 -= gameData.dmgPerClick;
-    document.getElementById("health1").innerHTML = gameData.health1;
-    document.getElementById("health1").style.width = gameData.health1 + "%"; // document.getElementById("attack").style.width = health + "px";
-    if (gameData.health1 <=0){
-        document. location. reload();
+  health1: 100,
+  health2: 100,
+  dmgPerClick: 3
+}
+
+var item = document.getElementById("fighterCardId");
+
+
+function fight1() {
+  gameData.health1 -= gameData.dmgPerClick;
+  document.getElementById("health1").innerHTML = gameData.health1;
+  document.getElementById("health1").style.width = gameData.health1 + "%"; // document.getElementById("attack").style.width = health + "px";
+  // document.getElementById("fighterCardId").style.animationName('shake');
+  item.animate(
+    [
+      { transform: 'translate(3px) rotate(3deg)' },
+    ],
+    {
+      duration: 100
     }
-    console.log("damage!");
+  );
+
+
+  if (gameData.health1 <= 0) {
+    document.location.reload();
   }
-  
-  function fight2() {
-    gameData.health2 -= gameData.dmgPerClick
-    document.getElementById("health2").innerHTML = gameData.health2 + " Health"
-  }
+  console.log("damage!");
+}
+
+function fight2() {
+  gameData.health2 -= gameData.dmgPerClick
+  document.getElementById("health2").innerHTML = gameData.health2 + " Health"
+}
 
   // ADD LISTENER FOR fight1()
   // ON CLICK REDUCE HP
