b35b1bcd8f852d5bc8deeb8df796832ee8cefa25
will-s-205-mac <will.step.205@gmail.com>
2022-08-02 20:57:29 -0700

    examples added

--- game-dev/fight-scene-3/main.js
+++ game-dev/fight-scene-3/main.js
@@ -1,18 +1,38 @@
 var gameData = {
   health1: 100,
   health2: 100,
-  dmgPerClick: 3
+  dmgPerClick: 30
 }
+
+// EXAMPLE
+const aliceTumbling = [
+  { transform: 'rotate(0) translate3D(-50%, -50%, 0)', color: '#000' },
+  { color: '#431236', offset: 0.3 },
+  { transform: 'rotate(360deg) translate3D(-50%, -50%, 0)', color: '#000' }
+];
+
+// EXAMPLE
+const aliceTiming = {
+  duration: 3000,
+  // iterations: Infinity
+}
+
+// EXAMPLE
+var doAnimation = document.getElementById("health1").animate(
+  aliceTumbling,
+  aliceTiming
+)
 
 function fight1() {
   gameData.health1 -= gameData.dmgPerClick;
   document.getElementById("health1").innerHTML = gameData.health1;
-  // document.getElementById("health1").style.backgroundColor = "blue";
+  // document.getElementById("health1").style.backgroundColor = "blue"; // EXAMPLE
   document.getElementById("health1").style.width = gameData.health1 + "%";
-  document.getElementsByClassName("healthbar1").animate(
-    [{ transform: 'translate(3px) rotate(3deg)'},],
+  document.getElementById("health1").animate(
+    [{ transform: 'translate(3px) rotate(3deg)' },],
     { duration: 100 }
   );
+
   if (gameData.health1 <= 0) {
     document.location.reload();
   }
